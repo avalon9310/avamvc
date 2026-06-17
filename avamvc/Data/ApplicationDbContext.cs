9998b794b402e705954954a2b8c9b312f7c51c49
avalon9310 <asd95027@gmail.com>
2026-06-17 23:14:03 +0800

    修正 PostgreSQL 的流水號與型態相容性

--- avamvc/Data/ApplicationDbContext.cs
+++ avamvc/Data/ApplicationDbContext.cs
@@ -9,5 +9,14 @@ namespace avamvc.Data {
         }
 
         public DbSet<Post> Posts { get; set; } // 資料庫模型
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            base.OnModelCreating(builder);
+
+            // 強制指定 Posts 的 Id 欄位在 PostgreSQL 中是自動遞增的流水號
+            builder.Entity<Post>()
+                .Property(p => p.Id)
+                .ValueGeneratedOnAdd();
+        }
     }
 }
--- avamvc/Program.cs
+++ avamvc/Program.cs
@@ -8,13 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options => {
-#if DEBUG
-    // 本機開發按下 F5 執行時，自動啟用 SQL Server
-    options.UseSqlServer(connectionString);
-#else
-        // 推送到 Git、Render 雲端打包時，自動啟用 PostgreSQL
+    //#if DEBUG
+    //    // 本機開發按下 F5 執行時，自動啟用 SQL Server
+    //    options.UseSqlServer(connectionString);
+    //#else
+    //            // 推送到 Git、Render 雲端打包時，自動啟用 PostgreSQL
     options.UseNpgsql(connectionString);
-#endif
+    //#endif
 });
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
@@ -45,4 +45,10 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
+using (var scope = app.Services.CreateScope()) {
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    // 只要資料表不存在，EF Core 就會根據目前的驅動（SQL Server 或 Postgres）自動建立最新結構！
+    context.Database.EnsureCreated();
+}
 app.Run();
